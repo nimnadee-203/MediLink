eae669d7c3f9d0517363bf64dcd9833db4efe7a1
Yasanga32 <yasangashanuka33@gmail.com>
2026-03-27 21:22:27 +0530

    designed model schemas added

---
+++ services/doctor-service/src/controllers/admin.controller.js
@@ -0,0 +1,25 @@
+import mockUsers from "../../mocks/users.mock"
+
+
+//API to get dashboard data for admin panel
+const adminDashboard = async (req,res)=>{
+    try{
+        const doctors = await mockUsers.find({})
+        const users = await mockUsers.find({})
+        const appoiments = await mockAppointments.find({})
+
+        const dashData = {
+            doctors:doctors.length,
+            appoiments:appoiments.length,
+            patients:users.length,
+            latestAppoiments: appoiments.reverse().slice(0,5)
+
+        }
+
+        res.json({success:true,dashData})
+
+    }catch(error){
+        console.log(error)
+        res.json({success:false,message:error.message})
+    }
+}
