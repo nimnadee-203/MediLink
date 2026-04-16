a9b146b2003adef3e0227a4e62a24cd491ec2e64
Yasanga32 <yasangashanuka33@gmail.com>
2026-04-16 17:40:19 +0530

    add kubernates

--- services/patient-service/src/server.js
+++ services/patient-service/src/server.js
@@ -22,7 +22,7 @@ app.use((req, res, next) => {
   res.header('Access-Control-Allow-Origin', '*');
   res.header(
     'Access-Control-Allow-Headers',
-    'Origin, X-Requested-With, Content-Type, Accept, Authorization, X-Clerk-Email, X-Clerk-Name, X-Clerk-Phone'
+    'Origin, X-Requested-With, Content-Type, Accept, Authorization, dtoken, Dtoken, atoken, Atoken, X-Clerk-Email, X-Clerk-Name, X-Clerk-Phone'
   );
   res.header('Access-Control-Allow-Methods', 'GET,POST,PUT,PATCH,DELETE,OPTIONS');
 
